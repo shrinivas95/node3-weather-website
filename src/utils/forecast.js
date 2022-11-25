9981da85adb9f181be18ded62e4b493a66033fca
shrinivas95 <shrinivas.xworkz@gmail.com>
2022-11-25 13:02:34 +0530

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=216d27998f1db378b7442a8c25c321d1&query='+ latitude +','+ longitude +'&units=f'
+    
+    request({ url , json : true},(error, { body })=>{
+        if(error){
+            callback('Unable to connect to weather service!', undefined)
+        }else if(body.error){  
+            callback('Unable to find location!', undefined)
+        }else {
+            callback(undefined,body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature +' degrees out,'+' It feels like '+body.current.feelslike)
+        }
+    })
+}
+
+module.exports = forecast
